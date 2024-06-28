03a387c383556f0e6c8f1895d98bfd08b1a88662
Xiang Chen <iflygo@outlook.com>
2024-06-28 10:16:10 +0800

    chore: add base conf, copy from Lazynvim

--- lua/neome/base/init.lua
+++ lua/neome/base/init.lua
@@ -1,14 +1,7 @@
 local M = {}
 
-vim.g.encoding = "utf-8"
-vim.o.fileencoding = "utf-8"
-
-vim.wo.number = true
-
-vim.o.backup = false
-vim.o.writebackup = false
-vim.o.swapfile = false
-
-vim.o.pumheight = 8
+require "neome.base.options"
+require "neome.base.keymaps"
+require "neome.base.autocmds"
 
 return M
---
+++ lua/neome/base/options.lua
@@ -0,0 +1,20 @@
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+vim.g.encoding = "utf-8"
+vim.o.fileencoding = "utf-8"
+
+vim.wo.number = true
+
+vim.o.backup = false
+vim.o.writebackup = false
+vim.o.swapfile = false
+
+vim.o.pumheight = 8
+
+vim.opt.autowrite = true
+vim.opt.cursorline = true
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+
